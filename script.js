d43c1a40325ea0db97839eec77c01dca80f6b4cb
Ronny4ek <90521844+Ronny4ek@users.noreply.github.com>
2021-11-05 18:33:06 +0200

    DOM Intro

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function fib(n) {
-    if (n==1||n==2) 
-        return 1;
-    else 
-        return fib(n-2)+fib(n-1);
+function textConsole() {
+    let inputValue = document.getElementById("inputText").value;
+    console.log(inputValue);
+    document.getElementById("inputText").value = "";
 }
-
-var userNum=+prompt('Введите порядковый номер числа Фибоначчи');
-alert(fib(userNum));
